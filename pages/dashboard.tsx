ae331ae8eed2e399b721f343517ac5aa93403429
WilliamMokoena <swmokoena412@gmail.com>
2022-01-31 07:57:15 +0000

    Removed Material UI

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,30 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
-import AppBar from "Components/AppBar"
-import Drawer from "Components/Drawer"
-import Grid from '@mui/material/Grid'
-
 const Dashboard: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>Dashboard</title>
-        <meta name="description" content="Dashboard" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <AppBar />
-        </Grid>
-        <Grid item xs={4}>
-          <Drawer />
-        </Grid>
-        <Grid item xs={8}>
-          <div>Hello Dashboard</div>
-        </Grid>
-      </Grid>
-
+      <h1>Hello Dashboard</h1>
     </div>
   )
 }
