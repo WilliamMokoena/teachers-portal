5b574dda6300f18a549dbee45d31ec126ea30fd9
WilliamMokoena <swmokoena412@gmail.com>
2022-02-11 16:49:26 +0000

    Added basic form control to AuthControl Components

--- components/AuthControl/signInForm.tsx
+++ components/AuthControl/signInForm.tsx
@@ -18,7 +18,16 @@ import {
   Input,
 } from '@chakra-ui/react'
 
+import { useForm } from 'react-hook-form'
+
 const SignInForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
   return (
     <Flex w="full" h="full" justifyContent="center" alignItems="center">
       <Box
@@ -30,58 +39,73 @@ const SignInForm = () => {
         paddingX="6"
         paddingY="10"
       >
-        <VStack spacing={12}>
-          {/* Form Header */}
-          <VStack spacing={6}>
-            <Avatar
-              size="2xl"
-              name="Jeffery Erhunse"
-              src="https://images.unsplash.com/photo-1589156191108-c762ff4b96ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1372&q=80"
-            />
-            <Flex direction="column" alignItems="center">
-              <Text
-                fontSize="2xl"
-                color="gray.900"
-                style={{ fontWeight: 'bold' }}
-              >
-                Welcome Back, Tsatsi
-              </Text>
-              <Text fontSize="md" color="gray.700">
-                Please login to continue
-              </Text>
-            </Flex>
-          </VStack>
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data)
+          })}
+        >
+          <VStack spacing={12}>
+            {/* Form Header */}
+            <VStack spacing={6}>
+              <Avatar
+                size="2xl"
+                name="Jeffery Erhunse"
+                src="https://images.unsplash.com/photo-1589156191108-c762ff4b96ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1372&q=80"
+              />
+              <Flex direction="column" alignItems="center">
+                <Text
+                  fontSize="2xl"
+                  color="gray.900"
+                  style={{ fontWeight: 'bold' }}
+                >
+                  Welcome Back, Tsatsi
+                </Text>
+                <Text fontSize="md" color="gray.700">
+                  Please login to continue
+                </Text>
+              </Flex>
+            </VStack>
 
-          {/* Form Inputs */}
-          <VStack
-            justifyContent="flex-start"
-            w="80%"
-            alignItems="center"
-            spacing={6}
-          >
-            {/* Email */}
-            {/* <FormControl>
-              <FormLabel htmlFor="email-or-phone">Email or phone</FormLabel>
-              <Input id="email-or-phone" placeholder="me@example.com" />
-            </FormControl> */}
-            {/* Password */}
-            <FormControl>
-              <FormLabel htmlFor="password">Password</FormLabel>
-              <Input id="password" placeholder="******" type="password" />
-            </FormControl>
-          </VStack>
+            {/* Form Inputs */}
+            <VStack
+              justifyContent="flex-start"
+              w="80%"
+              alignItems="center"
+              spacing={6}
+            >
+              {/* Email */}
+              <FormControl>
+                <FormLabel htmlFor="email">Email or phone</FormLabel>
+                <Input
+                  {...register('email', { required: true })}
+                  id="email"
+                  placeholder="me@example.com"
+                />
+              </FormControl>
+              {/* Password */}
+              <FormControl>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <Input
+                  {...register('password', { required: true })}
+                  id="password"
+                  placeholder="******"
+                  type="password"
+                />
+              </FormControl>
+            </VStack>
 
-          {/* Form Action Buttons */}
-          <HStack w="80%">
-            <Button colorScheme="twitter" variant="ghost">
-              Switch Accounts
-            </Button>
-            <Spacer />
-            <Button colorScheme="twitter" variant="solid">
-              Sign In
-            </Button>
-          </HStack>
-        </VStack>
+            {/* Form Action Buttons */}
+            <HStack w="80%">
+              <Button colorScheme="twitter" variant="ghost">
+                Switch Accounts
+              </Button>
+              <Spacer />
+              <Button colorScheme="twitter" variant="solid" type="submit">
+                Sign In
+              </Button>
+            </HStack>
+          </VStack>
+        </form>
       </Box>
     </Flex>
   )
--- components/AuthControl/signUpForm.tsx
+++ components/AuthControl/signUpForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { Flex, Box, HStack, Text, VStack, Spacer } from '@chakra-ui/react'
 
 import {
@@ -10,74 +12,120 @@ import {
   Input,
 } from '@chakra-ui/react'
 
+import { useForm } from 'react-hook-form'
+
 const SignUpForm = () => {
+  const [passwordShown, setPasswordShown] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
   return (
     <Flex w="full" h="full" justifyContent="center" alignItems="center">
-      <Box
-        w="3xl"
-        borderWidth="1px"
-        borderRadius="lg"
-        backgroundColor="white"
-        bgColor=""
-        paddingX="6"
-        paddingY="10"
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data)
+        })}
       >
-        <Text fontSize="2xl" mb="8">
-          Create your account
-        </Text>
-        <HStack spacing="8">
-          <Box w="75%" backgroundColor="">
-            <VStack spacing="6">
-              <HStack spacing="6" w="full">
-                <FormControl>
-                  <FormLabel htmlFor="first-name">First name</FormLabel>
-                  <Input id="first-name" placeholder="" type="text" />
-                </FormControl>
-                <FormControl>
-                  <FormLabel htmlFor="last-name">Last name</FormLabel>
-                  <Input id="last-name" placeholder="" type="text" />
-                </FormControl>
-              </HStack>
-              <FormControl>
-                <FormLabel htmlFor="email">Email</FormLabel>
-                <Input id="email" placeholder="" type="email" />
-              </FormControl>
-              <Flex justifyContent="left" direction="column" w="full">
-                <HStack spacing="6" w="full" mb="4">
-                  <FormControl>
-                    <FormLabel htmlFor="password">Password</FormLabel>
-                    <Input id="password" placeholder="******" type="password" />
-                  </FormControl>
-                  <FormControl>
-                    <FormLabel htmlFor="confirm-password">
-                      Confirm password
-                    </FormLabel>
+        <Box
+          w="3xl"
+          borderWidth="1px"
+          borderRadius="lg"
+          backgroundColor="white"
+          bgColor=""
+          paddingX="6"
+          paddingY="10"
+        >
+          <Text fontSize="2xl" mb="8">
+            Create your account
+          </Text>
+          <HStack spacing="8">
+            <Box w="75%" backgroundColor="">
+              <VStack spacing="6">
+                <HStack spacing="6" w="full">
+                  <FormControl isRequired>
+                    <FormLabel htmlFor="first-name">First name</FormLabel>
                     <Input
-                      id="confirm-password"
-                      placeholder="******"
-                      type="password"
+                      {...register('firstName', { required: true })}
+                      id="first-name"
+                      placeholder=""
+                      type="text"
+                    />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormLabel htmlFor="last-name">Last name</FormLabel>
+                    <Input
+                      {...register('lastName', { required: true })}
+                      id="last-name"
+                      placeholder=""
+                      type="text"
                     />
                   </FormControl>
                 </HStack>
-                <Checkbox colorScheme="twitter" value="1" ml="2">
-                  Show password
-                </Checkbox>
-              </Flex>
-              <HStack w="full" pt="10">
-                <Button colorScheme="twitter" variant="ghost">
-                  Sign in instead
-                </Button>
-                <Spacer />
-                <Button colorScheme="twitter" variant="solid">
-                  Next
-                </Button>
-              </HStack>
-            </VStack>
-          </Box>
+                <FormControl isRequired>
+                  <FormLabel htmlFor="email">Email</FormLabel>
+                  <Input
+                    {...register('email', { required: true })}
+                    id="email"
+                    placeholder=""
+                    type="email"
+                  />
+                </FormControl>
+                <Flex justifyContent="left" direction="column" w="full">
+                  <HStack spacing="6" w="full" mb="4">
+                    <FormControl isRequired>
+                      <FormLabel htmlFor="password">Password</FormLabel>
+                      <Input
+                        {...register('password', { required: true })}
+                        id="password"
+                        placeholder="******"
+                        type={passwordShown ? 'text' : 'password'}
+                      />
+                    </FormControl>
+                    <FormControl isRequired>
+                      <FormLabel htmlFor="confirm-password">
+                        Confirm password
+                      </FormLabel>
+                      <Input
+                        id="confirm-password"
+                        placeholder="******"
+                        type={passwordShown ? 'text' : 'password'}
+                      />
+                    </FormControl>
+                  </HStack>
+                  <Checkbox
+                    colorScheme="twitter"
+                    value="1"
+                    ml="2"
+                    onChange={(e) =>
+                      setPasswordShown((prevState) => {
+                        return prevState ? false : true
+                      })
+                    }
+                  >
+                    Show password
+                  </Checkbox>
+                </Flex>
+                <HStack w="full" pt="10">
+                  <Button colorScheme="twitter" variant="ghost">
+                    Sign in instead
+                  </Button>
+                  <Spacer />
+                  <Button colorScheme="twitter" variant="solid" type="submit">
+                    Next
+                  </Button>
+                </HStack>
+              </VStack>
+            </Box>
 
-          <Box w="25%" backgroundColor="red.50"></Box>
-        </HStack>
-      </Box>
+            <Box w="25%" backgroundColor="red.50"></Box>
+          </HStack>
+        </Box>
+      </form>
     </Flex>
   )
 }
